34c8f00fa09bc57b5747d8d07febf517436346bb
Marinator89 <marine.kazemi@gmail.com>
2023-10-19 13:50:20 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path');
 
 module.exports = {
-  entry: './biblio/src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-};
+    mode: 'development',
+    entry: './biblio/src/index.js',
+    output: {
+      filename: 'bundle.js',
+      path: __dirname + '/biblio/dist'
+    }
+  };
