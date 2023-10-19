0ffc17b6d7a2dbc3762fa0e32747d272d5b96c6e
Marinator89 <marine.kazemi@gmail.com>
2023-10-19 23:11:49 +0200

    dernieres étapes

--- back-biblio/server.js
+++ back-biblio/server.js
@@ -1,19 +1,26 @@
 const express = require('express');
-const app = express();
-const port = 3000; // Vous pouvez utiliser un port de votre choix
-app.use(express.json());
-
-// Route racine pour accueillir les visiteurs
-app.get('/', (req, res) => {
-    res.json({ message: 'Bienvenue sur votre serveur API !' });
-});
-
-// Écoute du serveur sur le port spécifié
-app.listen(port, () => {
-    console.log(`Serveur API en cours d'exécution sur le port ${port}`);
-});
-
 const mysql = require('mysql2');
+const cors = require('cors');
+
+const swaggerJsDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const amqp = require('amqplib');
+const connection2 = await amqp.connect('amqp://localhost');
+
+const channel = await connection.createChannel();
+
+const queueName = 'ma-file-dattente';
+const message = 'Hello, RabbitMQ!';
+channel.assertQueue(queueName);
+channel.sendToQueue(queueName, Buffer.from(message));
+channel.consume(queueName, (message) => {
+  console.log(`Received message: ${message.content.toString()}`);
+});
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -22,7 +29,6 @@ const connection = mysql.createConnection({
     database: 'livredb'
 });
 
-// Connexion à la base de données MySQL
 connection.connect((err) => {
     if (err) {
         console.error('Erreur de connexion à la base de données : ' + err.stack);
@@ -31,16 +37,15 @@ connection.connect((err) => {
     console.log('Connecté à la base de données MySQL.');
 });
 
-const cors = require('cors');
+app.use(express.json());
+app.use(cors());
 
-const corsOptions = {
-    origin: '*', // Remplacez par le domaine de votre application Angular
-    optionsSuccessStatus: 200 // Certains navigateurs anciens (IE11, divers SmartTVs) gèrent mal le code 204
-};
+app.get('/', (req, res) => {
+    res.json({ message: 'Bienvenue sur votre serveur API !' });
+});
 
-// Récupération de tous les livres depuis la base de données
-app.get('/livres', cors(corsOptions), (req, res) => {
-    connection.query('SELECT * FROM livres', (error, results, fields) => {
+app.get('/livres', (req, res) => {
+    connection.query('SELECT * FROM livres', (error, results) => {
         if (error) {
             console.error('Erreur lors de la récupération des livres : ' + error.message);
             res.status(500).json({ message: 'Erreur serveur' });
@@ -51,8 +56,7 @@ app.get('/livres', cors(corsOptions), (req, res) => {
     });
 });
 
-// Récupération d'un livre par son ID depuis la base de données
-app.get('/livres/:id', cors(corsOptions), (req, res) => {
+app.get('/livres/:id', (req, res) => {
     const bookId = req.params.id;
     connection.query('SELECT * FROM livres WHERE id = ?', [bookId], (error, results) => {
         if (error) {
@@ -65,8 +69,7 @@ app.get('/livres/:id', cors(corsOptions), (req, res) => {
     });
 });
 
-// Ajout d'un nouveau livre dans la base de données
-app.post('/livres', cors(corsOptions), (req, res) => {
+app.post('/livres', (req, res) => {
     const newBook = req.body;
     connection.query('INSERT INTO livres SET ?', newBook, (error, results) => {
         if (error) {
@@ -79,8 +82,7 @@ app.post('/livres', cors(corsOptions), (req, res) => {
     });
 });
 
-// Mise à jour d'un livre existant dans la base de données
-app.put('/livres/:id', cors(corsOptions), (req, res) => {
+app.put('/livres/:id', (req, res) => {
     const bookId = req.params.id;
     const updatedBook = req.body;
     connection.query('UPDATE livres SET ? WHERE id = ?', [updatedBook, bookId], (error, results) => {
@@ -94,7 +96,6 @@ app.put('/livres/:id', cors(corsOptions), (req, res) => {
     });
 });
 
-// Suppression d'un livre par son ID depuis la base de données
 app.delete('/livres/:id', (req, res) => {
     const bookId = req.params.id;
     connection.query('DELETE FROM livres WHERE id = ?', [bookId], (error, results) => {
@@ -108,13 +109,9 @@ app.delete('/livres/:id', (req, res) => {
     });
 });
 
-
-const express = require('express');
-const swaggerUi = require('swagger-ui-express');
-const swaggerDocument = require('./swagger.json'); // Remplacez par le chemin vers votre fichier Swagger
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
-app.listen(port, () => {
+app.listen(5000, () => {
     console.log(`Serveur API en cours d'exécution sur le port ${port}`);
 });
+
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
